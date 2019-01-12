9865c7f635427ded0384abf21df363e245fe2b63
Felix Navas <felixmorillasnavas@gmail.com>
2019-01-12 13:19:27 +0100

    PDF converter folder

---
+++ PDFconverter/convertPDF.py
@@ -0,0 +1,52 @@
+from cStringIO import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import os
+import sys, getopt
+
+#converts pdf, returns its text content as a string
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = file(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    return text 
+   
+# Now that we have a way to get the text content of a PDF, all we have to do is
+
+# Iterate through all of our PDFs.
+# For each pdf, get the text content,
+# open/create a .txt file,
+# write the text content to the .txt file.
+# You can do this using the following function and calling it like so:
+
+#converts all pdfs in directory pdfDir, saves all resulting txt files to txtdir
+def convertMultiple(pdfDir, txtDir):
+    if pdfDir == "": pdfDir = os.getcwd() + "\\" #if no pdfDir passed in 
+    for pdf in os.listdir(pdfDir): #iterate through pdfs in pdf directory
+        fileExtension = pdf.split(".")[-1]
+        if fileExtension == "pdf":
+            pdfFilename = pdfDir + pdf 
+            text = convert(pdfFilename) #get string of text content of pdf
+            textFilename = txtDir + pdf + ".txt"
+            textFile = open(textFilename, "w") #make text file
+            textFile.write(text) #write text to text file
+
+text = convert("data2.pdf")
+
+print(text)
